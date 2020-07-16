41fe4ba1922616036054c8f66ad2c4ff007ecf77
Alex Novaes <alex-novaes@hotmail.com>
2020-07-16 00:06:14 -0300

    added router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import Routes from './router/router.js'
 
 ReactDOM.render(
-    <App />,
+    <Routes />,
   document.getElementById('root')
 );
 
